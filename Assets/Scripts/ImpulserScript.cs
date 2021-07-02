2f220e90b2fb10b948e613c6e570c385f85fdc11
Greg Bahm <grbahm@microsoft.com>
2021-07-01 18:24:48 -0700

    Removed uncessary files.

--- Assets/Scripts/ImpulserScript.cs
+++ Assets/Scripts/ImpulserScript.cs
@@ -9,25 +9,8 @@ public class ImpulserScript : MonoBehaviour
     [SerializeField]
     private FluidSimulator simulator;
 
-    private Vector3 lastPos;
-
-
-	// Use this for initialization
-	void Start ()
-    {
-        lastPos = transform.localPosition;
-	}
-	
-	// Update is called once per frame
 	void Update ()
     {
         simulator.AddImpulse(transform.localPosition, transform.forward * ForceMultiplier);
-        //Vector3 movement = (transform.localPosition - lastPos);
-        //float distChange = movement.sqrMagnitude;
-		//if(distChange > float.Epsilon)
-        //{
-        //    simulator.AddImpulse(transform.localPosition, movement * ForceMultiplier);
-        //}
-        //lastPos = transform.localPosition;
 	}
 }
